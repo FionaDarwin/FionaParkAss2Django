234355a876bdca9ee937701f94aad1cee92d608c
FionaDarwin <f_park@iinet.net.au>
2022-06-20 07:33:24 +0930

    Update 20/6/22

--- chatbot/mychatbot/urls.py
+++ chatbot/mychatbot/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 #the dot above means "from within the same directory"
 
 urlpatterns = [
     path('', views.hi, name = 'home-page'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
